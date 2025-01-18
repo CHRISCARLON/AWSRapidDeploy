65321ba0b29b91ffed7d785db34efbab0ed794df
Chris Carlon <cmcarleone@gmail.com>
2025-01-18 11:09:45 +0000

    added ability to handle csv and parquet files

--- DuckDBLambda/lambda_function.py
+++ DuckDBLambda/lambda_function.py
@@ -1,11 +1,30 @@
-from typing import Dict, Any
 import duckdb
-from deltalake import write_deltalake
 import pyarrow as pa
+from typing import Dict, Any
+from deltalake import write_deltalake
+
+def handle_file_type(conn: duckdb.DuckDBPyConnection, extension: str, input_path: str) -> pa.Table:
+    match extension:
+        case "parquet":
+            return conn.query(f"""
+                SELECT *
+                FROM read_parquet('{input_path}')
+                LIMIT 100
+            """).arrow()
+        case "csv":
+            return conn.query(f"""
+                SELECT *
+                FROM read_csv('{input_path}')
+                LIMIT 100
+            """).arrow()
+        case _:
+            raise ValueError(f"Unsupported file type: {extension}")
 
 def handler(event: Dict[str, Any], context: Any) -> Dict[str, Any]:
     """
     Lambda handler for processing S3 events and writing to Delta table.
+
+    Supports parquet and csv files.
     
     Args:
         event: S3 event trigger
@@ -18,6 +37,10 @@ def handler(event: Dict[str, Any], context: Any) -> Dict[str, Any]:
     # Get bucket and key from the S3 event
     bucket: str = event["Records"][0]["s3"]["bucket"]["name"]
     key: str = event["Records"][0]["s3"]["object"]["key"]
+
+    # Extract file extension
+    extension: str = key.split('.')[-1].lower()
+    print(f"File extension is: {extension}")
     
     # Initialise DuckDB connection in memory
     conn = duckdb.connect(':memory:')
@@ -40,15 +63,12 @@ def handler(event: Dict[str, Any], context: Any) -> Dict[str, Any]:
     output_delta_path: str = "s3://duckdb-lambda-delta-bucket/delta-table"
 
     # Read parquet file into Arrow table
-    df: pa.Table = conn.query(f"""
-        SELECT *
-        FROM read_parquet('{input_path}')
-        LIMIT 100
-    """).arrow()
+    df: pa.Table = handle_file_type(conn, extension, input_path)
 
     print("ARROW TABLE CREATED")
     print(df.take([0,1,2,3,4]).to_pylist())
     print("Total rows:", len(df))
+    print(df.describe())
     
     # Write to Delta table
     write_deltalake(
